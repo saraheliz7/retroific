c9bf19b59ddd0ab9ec6d8cf9b5e9c47c0bb49f73
saraheliz7 <woodsose@gmail.com>
2018-03-31 13:25:53 -0400

    Created basic project structure and test set up.

---
+++ src/test/java/com/sarahbeach/retroific/ExampleTest.java
@@ -0,0 +1,14 @@
+package com.sarahbeach.retroific;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ExampleTest {
+
+    @Test
+    public void alwaysTrue() {
+        assertThat(true).isEqualTo(true);
+    }
+
+}
