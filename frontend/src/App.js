b8f14b1d13a7cb905ea8b915b9260f03ed97a4df
saraheliz7 <woodsose@gmail.com>
2018-04-06 17:09:17 -0400

    Added basic component to add board

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
+import BoardService from './BoardService';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {boardName: ''};
+        this._boardService = new BoardService();
+    }
+    createBoard() {
+        this._boardService.createBoard(this.state.boardName);
+    }
+    setBoardName(name) {
+        this.setState({boardName: name});
+    }
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <div className="title-container d-flex h-100 p-3 mx-auto flex-column">
+            <header className="title mb-auto">
+                <div>
+                    <h3 className="title-brand text-left">RETROIFIC</h3>
+                </div>
+            </header>
+            <main role="main" className="inner cover text-center">
+                <h1 className="retro-heading">Create a Retro Board</h1>
+                <p className="lead">Help your weekly Retro's go smoothly. Just give it a name and you're off!</p>
+                <div>
+                    <div className="container">
+                        <div className="row justify-content-center">
+                            <div className="col-8">
+                                <form>
+                                    <div className="form-group">
+                                        <label className="text-left">Pick a Name</label>
+                                        <input type="text" className="form-control" id="board-name" onChange={(event) => this.setBoardName(event.target.value)} placeholder="Your Board Name"/>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="lead">
+                    <button className="btn btn-lg btn-secondary add-board" onClick={() => this.createBoard()}>Create Board</button>
+                </div>
+            </main>
+        </div>
     );
   }
 }
--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import {configure, shallow} from "enzyme";
+import Adapter from 'enzyme-adapter-react-16';
+import BoardService from './BoardService';
+configure({ adapter: new Adapter() });
+jest.mock('./BoardService');
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+
+describe('App component', () => {
+    it('creates a board when create board button is clicked', () => {
+        const subject = shallow(<App/>);
+        subject.find("#board-name").simulate("change", {target: {value:"Things About Jeremy"}});
+        subject.find(".add-board").simulate("click");
+        const [mockInstance] = BoardService.mock.instances;
+        expect(mockInstance.createBoard).toHaveBeenCalledWith("Things About Jeremy");
+    });
 });
+
