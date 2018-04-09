b3ccf6543288a3bb76796cd3a45f556377287f0e
Nicholas Beach <nicholascbeach@gmail.com>
2018-04-09 17:57:24 -0400

    Added mybatis/flyway setup for persistance

--- src/main/java/com/sarahbeach/retroific/Application.java
+++ src/main/java/com/sarahbeach/retroific/Application.java
@@ -2,9 +2,10 @@ package com.sarahbeach.retroific;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:retroific.properties")
 public class Application {
 
     public static void main(String[] args) {
